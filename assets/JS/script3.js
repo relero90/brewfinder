f0ec9af2a255a1593792e09f8a4e5ac64b4ba400
relero90 <relero90@gmail.com>
2022-08-18 14:15:38 -0600

    header image displays on load, disappears on click

--- assets/JS/script3.js
+++ assets/JS/script3.js
@@ -1,2 +1,8 @@
-var welcomeImg = $("welcome-image");
-var appBody = $("appBody");
+var welcomeImg = document.querySelector("#welcome-image");
+var appBody = document.querySelector("#appBody");
+
+document.addEventListener("click", function () {
+  console.log(appBody);
+  appBody.classList.remove("hide");
+  welcomeImg.classList.add("hide");
+});
