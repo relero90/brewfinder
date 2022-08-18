a7362a7872e3329bf8955007da9e4f4103738739
relero90 <relero90@gmail.com>
2022-08-18 17:43:37 -0600

    fixed problems with spaces in city inputs and search history button function

--- assets/JS/script3.js
+++ assets/JS/script3.js
@@ -2,7 +2,6 @@ var welcomeImg = document.querySelector("#welcome-image");
 var appBody = document.querySelector("#appBody");
 
 document.addEventListener("click", function () {
-  console.log(appBody);
   appBody.classList.remove("hide");
   welcomeImg.classList.add("hide");
 });
